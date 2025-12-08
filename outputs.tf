00e939cbc709bbfb64c397517a65f1485a4cdaf9
Yu Feng <fengyu225@gmail.com>
2025-12-07 22:22:15 -0800

    create iam role to bypass rds access managed by spire federated iam role

--- outputs.tf
+++ outputs.tf
@@ -37,3 +37,7 @@ output "auditor_console_url" {
   description = "AWS Console Sign-in Link"
   value       = "https://${data.aws_caller_identity.current.account_id}.signin.aws.amazon.com/console"
 }
+
+output "bypass_role_arn" {
+  value = aws_iam_role.bypass.arn
+}
